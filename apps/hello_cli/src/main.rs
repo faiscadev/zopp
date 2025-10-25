d682bfc0fa2c3f26a228a88ccadb92ca3683dc25
Lucas Vieira <vieiralucas4@gmail.com>
2025-10-25 17:18:32 -0300

    ci

--- apps/hello_cli/src/main.rs
+++ apps/hello_cli/src/main.rs
@@ -1,6 +1,8 @@
 use hello_core::greet;
 
 fn main() {
-    let name = std::env::args().nth(1).unwrap_or_else(|| "world".to_string());
+    let name = std::env::args()
+        .nth(1)
+        .unwrap_or_else(|| "world".to_string());
     println!("{}", greet(&name));
 }
